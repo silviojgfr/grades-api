8cc4ffba77cf1783cf862b103314b8fdc4c76570
silvio.ribeiro <silviojgfr@gmail.com.ribeiro>
2020-09-04 23:54:44 -0300

    Commit parcial. gradeModel e integração com objeto DB exportado

---
+++ models/gradeModel.js
@@ -0,0 +1,30 @@
+// Criamos uma função (arrow) que recebe com parâmetro o mongoose, cria o schema para a collection 'accounts',
+// a partir desse schema, cria o modelo 'Account', retornando esse modelo.
+export default (mongoose) => {
+  const gradeSchema = mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    lastModified: {
+      type: Date,
+      required: true,
+    },
+  });
+
+  const Grade = mongoose.model('grades', gradeSchema, 'grades');
+
+  return Grade;
+};
